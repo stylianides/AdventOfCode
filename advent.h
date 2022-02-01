3902fd9c3efacc06f806679a517c9362d835c3ab
Stylianides <parisstylian@gmail.com>
2022-02-01 16:17:29 +0200

    2015 Day 1

---
+++ advent.h
@@ -0,0 +1,24 @@
+
+#ifndef ADVENT_H
+#define ADVENT_H
+
+#include <cstdint>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int32 bool32;
+
+typedef float real32;
+typedef double real64;
+
+typedef size_t mem_index;
+
+#endif //ADVENT_H
